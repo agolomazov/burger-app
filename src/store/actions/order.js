bb72ced257dd790f32d733c123b0e6524d0bac8b
Антон Голомазов <golomazov-tosha2012@yandex.ru>
2018-01-14 19:56:23 +0300

    Refactoring reducers

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -4,7 +4,7 @@ import axios from '../../axios-orders';
 export const purchaseBurgerSuccess = (id, orderData) => {
   return {
     type: actionTypes.PURCHASE_BURGER_SUCCESS,
-    orderId: id,
+    id,
     orderData: orderData
   }
 };
@@ -34,3 +34,35 @@ export const purchaseBurger = (orderData) => {
 export const purchaseInit = () => ({
   type: actionTypes.PURCHASE_INIT
 });
+
+export const fetchOrdersSuccess = (orders) => ({
+  type: actionTypes.FETCH_ORDERS_SUCCESS,
+  orders
+});
+
+export const fetchOrdersFail = (error) => ({
+  type: actionTypes.FETCH_ORDERS_FAIL,
+  error
+});
+
+export const fetchOrdersStart = () => ({
+  type: actionTypes.FETCH_ORDERS_START
+})
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart())
+    axios.get('/orders.json').then(response => {
+      const fetchedOrders = [];
+      for(let key in response.data) {
+        fetchedOrders.push({
+          ...response.data[key],
+          id: key
+        })
+      }
+      dispatch(fetchOrdersSuccess(fetchedOrders))
+    }).catch(error => {
+      dispatch(fetchOrdersFail(error))
+    })
+  }
+}
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import {updateObject} from '../utility';
 
 const initialState = {
   orders: [],
@@ -6,34 +7,50 @@ const initialState = {
   purchasing: false
 };
 
+const purchaseInit = (state, action) => {
+  return updateObject(state, {purchasing: false});
+};
+
+const purchaseBurgerStart = (state, action) => {
+  return updateObject(state, {loading: true});
+};
+
+const purchaseBurgerSuccess = (state, action) => {
+  return updateObject(state, {
+    loading: false,
+    orders: state.orders.concat({
+      orderId: action.orderId,
+      orderData: action.orderData.orderData,
+      ingredients: action.orderData.ingredients,
+      price: action.orderData.price
+    }),
+    purchasing: true
+  });
+};
+
+const fetchOrdersStart = (state, action) => {
+  return updateObject(state, {loading: true});
+};
+
+const fetchOrdersSuccess = (state, action) => {
+  return updateObject(state, {
+    orders: action.orders,
+    loading: false
+  });
+};
+
+const fetchOrdersFail = (state, action) => {
+  return updateObject(state, {loading: false});
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.PURCHASE_INIT:
-      return {
-        ...state,
-        purchasing: false
-      };
-    case actionTypes.PURCHASE_BURGER_START:
-      return {
-        ...state,
-        loading: true
-      }
-    case actionTypes.PURCHASE_BURGER_SUCCESS:
-      return {
-        ...state,
-        loading: false,
-        orders: state.orders.concat({
-          orderId: action.orderId,
-          orderData: action.orderData
-        }),
-        purchasing: true
-      };
-    case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        loading: false,
-        error: action.error
-      };
+    case actionTypes.PURCHASE_INIT: return purchaseInit(state, action);
+    case actionTypes.PURCHASE_BURGER_START: return purchaseBurgerStart(state, action);
+    case actionTypes.PURCHASE_BURGER_SUCCESS: return purchaseBurgerSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_START: return fetchOrdersStart(state, action);
+    case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+    case actionTypes.FETCH_ORDERS_FAIL: return fetchOrdersFail(state, action);
     default:
       return state;
   }
